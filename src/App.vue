2d36d28f5b7cade7ac5338cd117badff88ce4727
J Ko <just.jai.ko@gmai.com>
2023-04-03 19:03:05 -0700

    input all the info

--- src/App.vue
+++ src/App.vue
@@ -4,20 +4,302 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <div class="background">
+    <div class="paper">
+      <div class="col-1">
+        <div class="section main">
+          <h1 class="name">J Ko</h1>
+          <h2>Software Developer</h2>
+          <ul class="contact-list">
+            <li>
+              <a href="">
+                <img src="" alt="" class="contact-ion" />
+                J-Ko.dev
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <img src="" alt="" class="contact-ion" />
+                206jko@gmail.com
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <img src="" alt="" class="contact-ion" />
+                +1.425.220.5899
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <img src="" alt="" class="contact-ion" />
+                Github.com/jko206
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <img src="" alt="" class="contact-ion" />
+                LinkedIn.com/in/jai-ko
+              </a>
+            </li>
+          </ul>
+        </div>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+        <div class="section">
+          <h2>Introduction</h2>
+          <div>
+            Passionate frontend engineer, building the bridge between computer and humans. A
+            computer is as good as how easy it is to use it, and my job is to make computing easy
+            for people, both on mobile and web.
+          </div>
+        </div>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+        <div class="section">
+          <h2>Education</h2>
+          <h3>University of Washington, Seatttle</h3>
+          <ul class="plain-list">
+            <li>Mathematics B.S.</li>
+            <li>Computer science focused</li>
+          </ul>
+        </div>
+
+        <div class="section">
+          <h2>Language / Frameworks</h2>
+          <ul class="slash-list">
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>TypeScript</li>
+            <li>Flow</li>
+            <li>ES6+</li>
+            <li>Vue</li>
+            <li>React</li>
+            <li>Redux</li>
+            <li>SASS</li>
+            <li>SCSS</li>
+            <li>jQuery</li>
+            <li>jest</li>
+            <li class="de-emphasized">GraphQL</li>
+            <li class="de-emphasized">RxJS</li>
+            <li class="de-emphasized">Google Web Toolkit(GWT)</li>
+            <li class="de-emphasized">PHP</li>
+            <li class="de-emphasized">Hack</li>
+            <li class="de-emphasized">Java</li>
+            <li class="de-emphasized">NodeJS</li>
+            <li class="de-emphasized">MySQL</li>
+          </ul>
+        </div>
+
+        <div class="section">
+          <h2>Concepts / Paradigms</h2>
+          <ul class="slash-list">
+            <li>OOP</li>
+            <li>Responsive design</li>
+            <li>SPA</li>
+            <li>Flux pattern</li>
+            <li>Hooks</li>
+            <li>Algorithm</li>
+            <li>Data structure</li>
+            <li>Functional programming</li>
+            <li>User interface (UI)</li>
+            <li>User experience (UX)</li>
+            <li>Internationalization (i18n)</li>
+            <li>REST API</li>
+            <li>Test driven design (TDD)</li>
+            <li>Unit testing</li>
+            <li>E2E testing</li>
+          </ul>
+        </div>
+
+        <div class="section">
+          <h2>Tools</h2>
+          <ul class="slash-list">
+            <li>npm</li>
+            <li>webpack</li>
+            <li>vite</li>
+            <li>Firebase</li>
+            <li>Babel</li>
+            <li>git</li>
+            <li>Vue-related tools</li>
+          </ul>
+        </div>
+
+        <div class="section">
+          <h2>MISC</h2>
+          <ul class="hash-list">
+            <li>
+              Places in the top percentiles in various JavaScript assessments, such as LinkedIn,
+              TopTal, TripleByte, etc.
+            </li>
+            <li>
+              Having been a private tutor for 9 years, I care deeply about how and why people
+              encounter hindrance in accessing information. This care has been translated into my
+              passion for creating intuitive UI/UX/translations of a web page
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="col-2">
+        <h2>Experience</h2>
+
+        <div class="section">
+          <div class="h3-row">
+            <h3>Rippling</h3>
+            <span class="title">Senior Software Engineer</span>
+          </div>
+          <div class="desc"></div>
+          <ul class="job-desc-list">
+            <li></li>
+          </ul>
+        </div>
+
+        <div class="section">
+          <div class="h3-row">
+            <h3>Meta: Web3 Services</h3>
+            <span class="title">Software Engineer / 2021.10 – Present</span>
+          </div>
+          <div class="desc">
+            Build the necessary tools as the company transitions from Web 2 to Web 3
+          </div>
+          <ul class="job-desc-list">
+            <li>
+              Create an internal tool similar to OpenSea.io that enabled users to view information
+              on NFT and blockchain addresses
+            </li>
+            <li>
+              Actively engage wide range of teams to scope out their needs and prioritize features
+              that had the largest impact to the business
+            </li>
+            <li>Write GraphQL queries that enables searching for NFTs by different attributes</li>
+          </ul>
+        </div>
+
+        <div class="section">
+          <div class="h3-row">
+            <h3>Amazon Web Services: EC2 Networking</h3>
+            <span class="title">Software Engineer / 2020.03 – 2021.10</span>
+          </div>
+          <div class="desc">
+            Develop the console and the infrastructure behind it for the Elastic Load Balancing org
+          </div>
+          <ul class="job-desc-list">
+            <li>Rebuild the console written in GWT with internally developed React components</li>
+            <li>
+              Use RxJS and Redux to manage the internal state of the app and to communicate with the
+              backend
+            </li>
+            <li>Write and maintain end-to-end (E2E) tests as well as component tests</li>
+            <li>
+              Respond to customer inquiries and requests, both internal and external, including
+              emergencies
+            </li>
+            <li>Monitor and maintain the CI/CD pipeline for smooth worldwide deployment</li>
+            <li>
+              Write and improve various internal docs that reduced onboarding time from 6 weeks to 1
+              week
+            </li>
+          </ul>
+        </div>
+
+        <div class="section">
+          <div class="h3-row">
+            <h3>Pilot.com</h3>
+            <span class="title">Software Engineer / 2019.03 – 2020.01</span>
+          </div>
+          <div class="desc">Develop an app for the internal bookkeepers</div>
+          <ul class="job-desc-list">
+            <li>
+              Overhaul the internal dashboard page that displays thousands of customer statuses with
+              details about their work completion, assignee, and other information
+            </li>
+            <li>
+              Develop / test pixel-perfect UI based on design using Vue / SASS / Jest / Vue test
+              util
+            </li>
+            <li>
+              Communicate with stakeholders such as designers and Product Specialists to improve
+              existing UI and initiate new projects
+            </li>
+            <li>Address bugs and comments with short turnarounds</li>
+            <li>
+              Make asynchronous (AJAX) calls to REST API via JavaScript Promise to update pages
+            </li>
+            <li>
+              Optimize pre-existing codebase by modernizing code base with the newest standard of
+              HTML/JavaScript/CSS and adding greater test coverage
+            </li>
+            <li>
+              Document progress, accomplishments, and technical specs for added features and
+              debugging
+            </li>
+          </ul>
+        </div>
+
+        <div class="section">
+          <div class="h3-row">
+            <h3>
+              A service that lets users practice infinite number of math problem in preparation for
+              standardized math tests.
+            </h3>
+            <span class="title">Full Stack Developer / 2018.12 – Present </span>
+          </div>
+          <div class="desc">
+            A service that lets users practice infinite number of math problem in preparation for
+            standardized math tests.
+          </div>
+          <ul class="job-desc-list">
+            <li>
+              Planned, designed, and coded it from scratch, based on 9 years of teaching experience
+            </li>
+            <li>Single Page Application (SPA); loads partial contents as needed</li>
+            <li>Collect user information and metrics with Firestore</li>
+            <li>Uses third party (Stripe) JS library and API calls to securely process payment</li>
+          </ul>
+        </div>
+
+        <h2>Projects</h2>
+
+        <div class="section">
+          <h3>Arraze</h3>
+          <div class="desc">A JavaScript utility library for array manipulation.</div>
+          <ul class="job-desc-list">
+            <li>Designed to be intuitive, light, tested, and well documented.</li>
+            <li>
+              Provides functionalities such as sort(), filter(), shuffle(), and createNumbered()
+              that are often used by developers, but implemented each time they're used.
+            </li>
+            <li>Written in TypeScript for optimal IDE integration and type safety</li>
+          </ul>
+        </div>
+
+        <div class="section">
+          <h3>DirTree</h3>
+          <div class="desc">An app that prints folder structures to be used in dev doc.</div>
+          <ul class="job-desc-list">
+            <li>Develop custom algorithm to display directory structure</li>
+            <li>Develop intuitive UI/UX to be used with keyboard</li>
+            <li>Collect user feedback and metrics with Firestore</li>
+          </ul>
+        </div>
+
+        <div class="section">
+          <h3>PrecisionJS</h3>
+          <div class="desc">
+            A JavaScript math library that allows high level math with infinite precision.
+          </div>
+          <ul class="job-desc-list">
+            <li>Uses array or BigInt calculate arbitrarily large numbers with precision</li>
+            <li>Parses and calculates repeating decimals and fractions</li>
+            <li>
+              Uses series, implemented in recursion or while loop, to run trigonometric functions
+              and higher-level functions with practically infinite precision
+            </li>
+            <li>Being re-written in TypeScript</li>
+          </ul>
+        </div>
+      </div>
     </div>
-  </header>
-
-  <RouterView />
+  </div>
 </template>
 
 <style scoped>
