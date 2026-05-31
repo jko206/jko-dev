dec7b96cca429973cf3ba8d026c615172ed3e13f
J. Ko <just.jai.ko@gmail.com>
2026-05-30 18:16:30 -0700

    style(controls): replace pointing hand icon with cursor click ripple icon

--- src/components/ResumeControls.vue
+++ src/components/ResumeControls.vue
@@ -24,9 +24,9 @@
       aria-label="Toggle Interactive Features"
     >
       <svg viewBox="0 0 24 24" class="control-icon">
-        <path
-          d="M12 12V4a2 2 0 0 0-4 0v8H6a2 2 0 0 0-2 2v3a6 6 0 0 0 6 6h2a6 6 0 0 0 6-6v-5a2 2 0 0 0-4 0v2"
-        ></path>
+        <path d="M3 3l7.07 16.97 2.51-7.39 7.39-2.51L3 3z"></path>
+        <path d="M13 3a10 10 0 0 0-10 10"></path>
+        <path d="M9 3a6 6 0 0 0-6 6"></path>
       </svg>
     </button>
   </div>
