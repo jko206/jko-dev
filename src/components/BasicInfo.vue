e1ce3d7f3d81152ce380814ac3f262f8850351b8
J. Ko <just.jai.ko@gmail.com>
2026-05-30 18:01:16 -0700

    feat: create BasicInfo component

---
+++ src/components/BasicInfo.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="section main">
+    <h1 class="name">{{ name }}</h1>
+    <h2>{{ role }}</h2>
+    <ul class="contact-list">
+      <li v-for="item in contact" :key="item.text">
+        <a target="_blank" :href="item.href">
+          {{ item.text }}
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { ContactItem } from '../data/resume'
+
+defineProps<{
+  name: string
+  role: string
+  contact: ContactItem[]
+}>()
+</script>
