b0b6fcfff5475b64836d333427fb9bc10e530d87
J. Ko <just.jai.ko@gmail.com>
2026-05-30 18:01:15 -0700

    feat: create ResumeControls component

---
+++ src/components/ResumeControls.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="controls flex justify-center space-x-5 p-2">
+    <button
+      @click="emit('update:isInteractive', false)"
+      :class="['control', { active: !isInteractive }]"
+    >
+      Paper
+    </button>
+    <button
+      @click="emit('update:isInteractive', true)"
+      :class="['control', { active: isInteractive }]"
+    >
+      Interactive
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  isInteractive: boolean
+}>()
+
+const emit = defineEmits<{
+  (e: 'update:isInteractive', val: boolean): void
+}>()
+</script>
